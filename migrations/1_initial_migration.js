4d386bed0ae4d6e024747cebd914396a2775c8c5
Lucas Botelho <lucas.vbotelho83@gmail.com>
2022-04-04 16:18:57 -0300

    feat: initial config of smart contract

---
+++ migrations/1_initial_migration.js
@@ -0,0 +1,5 @@
+// const OneMillionPixels = artifacts.require("OneMillionPixels");
+
+module.exports = function (deployer) {
+  // deployer.deploy(OneMillionPixels, "OneMillionPixels", "OMP", 10000);
+};
